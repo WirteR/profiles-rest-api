40a461d0bb80eb4cf5b0c1e794117e8d081850f2
WirteR <wirterok@gmail.com>
2020-01-23 20:31:07 +0200

    add helloAPIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 
 
 class HelloAPIView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         an_apiview = [
             'Uses HTTP method as function(get, post, path, put, delete)',
@@ -13,3 +18,30 @@ class HelloAPIView(APIView):
         ]
 
         return Response({'message': 'hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+
+        else: 
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST 
+            )
+
+    def put(self, request, pk=None):
+        """Handle updating objects"""
+        return Response({'method': 'PUT'})
+
+    def path(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete object"""
+        return Response({'method': 'DELETE'})
